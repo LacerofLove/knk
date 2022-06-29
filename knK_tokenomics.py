24493eed90d81e8d5a886ac07c42473562f465cc
LacerofLove <107947847+LacerofLove@users.noreply.github.com>
2022-06-29 15:15:52 +0200

    added files

---
+++ knK_tokenomics.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+
+#@st.cache
+def load_data():
+    df1 = pd.read_pickle('cost_df')
+    df2 = pd.read_pickle('cost_df_cumulative')
+    return df1, df2
+
+
+st.subheader('Daily $KAGE token emissions')
+cost_df, cost_df_cumulative= load_data()
+#df_T = df.T
+
+cost_df
+
+st.subheader('$KAGE token emissions (cumulative)')
+
+cost_df_cumulative['difference'] =cost_df_cumulative.sum(1)
+
+cost_df_cumulative
+
+# source = pd.DataFrame({
+#   'days': df.index,
+#   '$KAGE': df['cumulative']
+# })
+transformed_df = cost_df_cumulative.reset_index().melt('index')
+c = alt.Chart(transformed_df).mark_line().encode(
+    x='index',
+    y='value',
+    color='variable',
+    tooltip=['variable', 'value']    
+).interactive()
+
+
+
+st.altair_chart(c, use_container_width=True)
